7b8216ef3865b62355bac1f821875fa303c1d0d2
Brandon <bmbartel@asu.edu>
2017-11-01 12:52:56 -0700

    This is where I left off in my other attempt to make this app. Ran into issues since I named my app CoreMotion

--- CoreMotApplication/FirstViewController.swift
+++ CoreMotApplication/FirstViewController.swift
@@ -1,25 +1,25 @@
 //
 //  FirstViewController.swift
-//  CoreMotApplication
+//  coreMotion
 //
-//  Created by Brandon on 11/1/17.
+//  Created by Brandon on 10/29/17.
 //  Copyright © 2017 Arizona State University. All rights reserved.
 //
 
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
--- CoreMotApplication/SecondViewController.swift
+++ CoreMotApplication/SecondViewController.swift
@@ -1,25 +1,36 @@
 //
 //  SecondViewController.swift
-//  CoreMotApplication
+//  coreMotion
 //
-//  Created by Brandon on 11/1/17.
+//  Created by Brandon on 10/29/17.
 //  Copyright © 2017 Arizona State University. All rights reserved.
 //
 
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    var buttonSelected = ""
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        if let destVC = segue.destination as? AccelerationTableViewController
+        {
+            destVC.buttonSelected = buttonSelected.self
+        }
+        
+    }
 }
-
