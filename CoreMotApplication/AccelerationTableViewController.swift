b7f8a620b9709ca4e1e15c285060cc3206758dac
Brandon <bmbartel@asu.edu>
2017-11-15 20:19:48 -0700

    all files now. Before there was an issue with the location the files were saved. When I opened the app on my computer everything looked fine. On others, it was missing files.

---
+++ CoreMotApplication/AccelerationTableViewController.swift
@@ -0,0 +1,199 @@
+//
+//  AccelerationTableViewController.swift
+//  CoreMotApplication
+//
+//  Created by Brandon on 10/30/17.
+//  Copyright © 2017 Arizona State University. All rights reserved.
+//
+
+import UIKit
+import CoreMotion
+
+// This is the controller that didn't initially upload.
+
+class AccelerationTableViewController: UITableViewController {
+    // Defining variables. motionManager needs to be setup so that we can call it later in the code.
+    let motionManager = CMMotionManager()
+    var buttonSelected = ""
+    let interval = 0.1
+    var motionTimer : Timer?
+    // If the current value changes, and the count is less than 100, it will reload the data. If it is equal to 100, then the array is full and it will segue to MoreDetail: Just showing the last 100 values in an alternate view.
+    var currentValue = [0.0] {
+        didSet {
+            if currentValue.count < 100
+            {
+                tableView.reloadData()
+            }
+            if currentValue.count == 100
+            {
+                performSegue(withIdentifier: "MoreDetail", sender: self)
+            }
+        }
+    }
+    
+    // Setup motion. Utilizing same method as in class.
+    func motionSetup() {
+        // tell the device to update motion data every second
+        motionManager.deviceMotionUpdateInterval = self.interval
+        
+        // if calibration is required, show the movement display
+        motionManager.showsDeviceMovementDisplay = true
+    }
+    // Starting the motion timer in the same manner as in class.
+    func startMotionTimer()
+    {
+        // tell the device to start updating motion data
+        self.motionManager.startDeviceMotionUpdates(using: .xMagneticNorthZVertical)
+        
+        // check for timer in progress
+        if motionTimer != nil {
+            motionTimer!.invalidate()
+        }
+        
+        // create and start timer with handler block that updates labels in the view
+        self.motionTimer = Timer(fire: Date(), interval: self.interval, repeats: true, block: {(motionTimer) in
+            // This switch statement makes it so that we insert the acceleration value, in m/s^2, to the array. This value will be x,y, or z dependent on the buttonSelected on the previous view.
+            if let data = self.motionManager.deviceMotion {
+                switch self.buttonSelected {
+                case "X Accel":
+                    self.currentValue.insert(data.userAcceleration.x * 9.81, at: 0)
+                case "Y Accel":
+                    self.currentValue.insert(data.userAcceleration.y * 9.81, at: 0)
+                case "Z Accel":
+                    self.currentValue.insert(data.userAcceleration.z * 9.81, at: 0)
+                default:
+                    print("Missed Value")
+                }
+            }
+        }
+        )
+        
+        
+        
+        
+        // add the timer to our run loop
+        RunLoop.current.add(self.motionTimer!, forMode: .defaultRunLoopMode)
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // This line updates the navigation title to the button selected on the previous view.
+        navigationItem.title = buttonSelected
+        
+        
+    }
+    
+    // Same as used in class. This will bein the motion manager as soon as the view appears, so long as device COREmotion is available.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        // check if we're able to sense device motion
+        if motionManager.isDeviceMotionAvailable {
+            //prime motion variables
+            self.motionSetup()
+            
+            // start getting motion data
+            self.startMotionTimer()
+        } else {
+            // pop up with error
+            presentAlert(title: "Error", message: "Device Motion is not available.")
+        }
+    }
+    
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        // stop timer and motion updates if navigating away from view
+        if motionTimer != nil {
+            motionTimer!.invalidate()
+        }
+        
+        // Stop the device motion updates as soo as the view goes away (segue occured).
+        self.motionManager.stopDeviceMotionUpdates()
+        
+        super.viewDidDisappear(true)
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // Function for presenting the alert.
+    func presentAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Plot the total number of values in the array.
+        return currentValue.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        // Configure the cell...
+        
+        
+        // Create a new variable that takes the current valye at the current indexPath row
+        let accelerations = currentValue[indexPath.row]
+        
+        // Setting threshold for the current value. If the acceleration is greater than or equal to 5 m/s^2, then the phone is definitely in motion.
+        if abs(currentValue[indexPath.row]) >= 5
+        {
+            cell.textLabel?.textColor = UIColor.green
+        }
+        else
+        {
+            cell.textLabel?.textColor = UIColor.black
+        }
+        
+        // Send the value to the text of the cell.
+        cell.textLabel?.text? = String(accelerations)
+        
+        
+        return cell
+        
+    }
+    
+    
+    
+    
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    
+    
+    
+    
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let SplitVC = segue.destination as! MoreDetailTableViewController
+        // Send the values in this table view to a segued view. Currently this added view doesn't hold much value. But moving forward, it could be used for data manipulation (i.e. trying to calculate distance). Potentially for the final project.
+        
+        SplitVC.Values = self.currentValue
+        SplitVC.sensorType = "Accel"
+        // Stop the motion manager updates once segue has occured.
+        self.motionManager.stopDeviceMotionUpdates()
+    }
+    
+    
+}
---
+++ CoreMotApplication/MoreDetailTableViewController.swift
@@ -0,0 +1,83 @@
+//
+//  MoreDetailTableViewController.swift
+//  CoreMotApplication
+//
+//  Created by Brandon on 11/3/17.
+//  Copyright © 2017 Arizona State University. All rights reserved.
+//
+
+import UIKit
+
+// This view controller will display acceleration(x,y,z) or gyro(roll,pitch,yaw) depending on what the user has navigated to. In this controller, moving forward for the final project, we could attempt to make plots. Potentially change this to a split view controller. And could perform data manipulation to try to estimate distance from acceleration values.
+
+class MoreDetailTableViewController: UITableViewController {
+    
+    // Initialize variables, so that we can pass values over from the previous views.
+    var Values = [0.0]
+    var sensorType = ""
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Maybe conditionally add a button: If the acceleration values are the values being looked at, add a button entitled (Estimate Distance). Then this button will Segue to another view which will plot and display the distance estimates (Split View Controller). Add this button as soon as the program loads. And set-up a performsegue function for this button.
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        
+        
+    }
+    
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return Values.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "value", for: indexPath)
+        
+        // Configure the cell...
+        
+        
+        let collectedAcceleration = Values[indexPath.row]
+        
+        // Only utilize the thresholding functionality in this tab if we are looking at the accelerometer values. The gyro values will always plot in black.
+        if sensorType == "Accel"
+        {
+            if abs(Values[indexPath.row]) >= 5
+            {
+                cell.textLabel?.textColor = UIColor.green
+            }
+            else
+            {
+                cell.textLabel?.textColor = UIColor.black
+            }
+        }
+            
+        else {
+            cell.textLabel?.textColor = UIColor.black
+        }
+        
+        cell.textLabel?.text? = String(collectedAcceleration)
+        
+        
+        return cell
+    }
+    
+    
+}
